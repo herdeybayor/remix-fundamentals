b4f0a818969d624b375ab4c458ad57f410e449eb
herdeybayor <shereefdeen4real@gmail.com>
2024-10-20 05:31:39 +0100

    exercise 2

---
+++ exercise/02-data-loading/app/models/post.server.ts
@@ -0,0 +1,10 @@
+import { prisma } from "~/db.server";
+
+export async function getPostsListItems() {
+  return prisma.post.findMany({
+    select: {
+      title: true,
+      slug: true,
+    },
+  });
+}
---
+++ exercise/02-data-loading/prisma/migrations/20241020042211_posts/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `body` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `id` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `markdown` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `slug` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "slug" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "markdown" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Post" ("createdAt", "title", "updatedAt") SELECT "createdAt", "title", "updatedAt" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
