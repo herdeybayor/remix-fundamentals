a3404aea2585c6d16e09760ae8578e712db871dd
herdeybayor <shereefdeen4real@gmail.com>
2024-10-20 12:42:57 +0100

    exercise 5

--- exercise/05-mutations/app/routes/posts/admin/new.tsx
+++ exercise/05-mutations/app/routes/posts/admin/new.tsx
@@ -1,27 +1,57 @@
-// 🐨 implement the action function here.
-// 1. accept the request object
-// 2. get the formData from the request
-// 3. get the title, slug, and markdown from the formData
-// 4. call the createPost function from your post.model.ts
-// 5. redirect to "/posts/admin".
+import { Form, useActionData } from "@remix-run/react";
+import { type ActionArgs, json, redirect } from "@remix-run/server-runtime";
+
+import { createPost } from "~/models/post.server";
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const title = String(formData.get("title"));
+  const slug = String(formData.get("slug"));
+  const markdown = String(formData.get("markdown"));
+
+  const errors: Record<string, string> = {};
+
+  if (!title) {
+    errors.title = "Title is required";
+  }
+
+  if (!slug) {
+    errors.slug = "Slug is required";
+  }
+
+  if (!markdown) {
+    errors.markdown = "Markdown is required";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return json(errors as Record<"title" | "slug" | "markdown", string>, {
+      status: 400,
+    });
+  }
+
+  await createPost(title, slug, markdown);
+
+  return redirect("/posts/admin");
+}
+
 export default function NewPost() {
+  const errors = useActionData<typeof action>();
   return (
-    // 🐨 change this to a <Form /> component from @remix-run/react
-    // 🐨 and add method="post" to the form.
-    <form>
+    <Form method="post">
       <p>
         <label>
           Post Title:{" "}
           <input type="text" name="title" className={inputClassName} />
+          {errors?.title && <em className="text-red-500">{errors.title}</em>}
         </label>
       </p>
       <p>
         <label>
           Post Slug:{" "}
           <input type="text" name="slug" className={inputClassName} />
+          {errors?.slug && <em className="text-red-500">{errors.slug}</em>}
         </label>
       </p>
       <p>
@@ -33,6 +63,9 @@ export default function NewPost() {
           name="markdown"
           className={`${inputClassName} font-mono`}
         />
+        {errors?.markdown && (
+          <em className="text-red-500">{errors.markdown}</em>
+        )}
       </p>
       <p className="text-right">
         <button
@@ -42,6 +75,6 @@ export default function NewPost() {
           Create Post
         </button>
       </p>
-    </form>
+    </Form>
   );
 }
